a1436b822652e15944c4c108d7e99d7f5ef3fde2
Brian Xu <bxu@godaddy.com>
2016-07-19 14:28:44 -0700

    reformatted data to new format, updated functions to adjust for that

--- mockDB.py
+++ mockDB.py
@@ -4,9 +4,36 @@ client = MongoClient()
 client.mockDB.reviews.drop()
 coll = client.mockDB.reviews
 data = [
-  {'rating': '5.0', 'name': 'Fang', 'body': 'What an amazing foodie experience at Fang!'},
-  {'rating': '3.0', 'name': 'Fang', 'body': 'The serious was relatively slow and the main guy was very pushy about his suggestions.'},
-  {'rating': '5.0', 'name': 'Los Hermanos Taqueria', 'body': 'DELICIOUS! It is a little sketchy, just because it\'s literally INSIDE a liquor store, but the lady who took our order was very sweet and giving.'}
+  {
+    'name': 'Fang', 
+    'url': 'http://www.yelp.com/biz/fang-san-francisco-2',
+    'reviews': [
+      {
+        'body': 'What an amazing foodie experience at Fang!',
+        'rating': 5.0
+      },
+      {
+        'body': 'The serious was relatively slow and the main guy was very pushy about his suggestions.',
+        'rating': 3.0
+      }
+    ],
+    'menu_items' : [
+      'Fried Potstickers', 'Fried Onioncake', 'Steamed Pork Dumplings'
+    ]
+  },
+  {
+    'name': 'Los Hermanos Taqueria',
+    'url':'http://www.yelp.com/biz/los-hermanos-taqueria-san-francisco',
+    'reviews': [
+      {
+        'body': 'DELICIOUS! It is a little sketchy, just because it\'s literally INSIDE a liquor store, but the lady who took our order was very sweet and giving.',
+        'rating': 5.0
+      }
+    ],
+    'menu_items': [
+      'Super Burrito', 'Chicken Caeser Salad', 'Mexican Salads'
+    ]
+  }
 ]
 
 for datum in data:
