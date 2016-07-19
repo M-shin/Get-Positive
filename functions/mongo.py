7482c978558d5bacfe704f4862a07226b8b7551c
Brian Xu <bxu@godaddy.com>
2016-07-19 11:24:31 -0700

    added name field do mockDB, created functions to accessdb

--- functions/mongo.py
+++ functions/mongo.py
@@ -1,2 +1,40 @@
+from pymongo import MongoClient
+
+# returns a list of review objects which contain a certain keyword
 def search_by_keyword(keyword):
-  pass
+  client = MongoClient()
+  coll = client.mockDB.reviews
+  cursor = coll.find()
+  results = []
+
+  for item in cursor:
+    if keyword in item['body']:
+      results.append(item)
+
+  return results
+
+# returns a list of review objects which have a certain rating
+def search_by_rating(rating):
+  client = MongoClient()
+  coll = client.mockDB.reviews
+  cursor = coll.find()
+  results = []
+
+  for item in cursor:
+    if float(rating) == float(item['rating']):
+      results.append(item)
+
+  return results
+
+# returns a list of review objects which is for a certain restaurant
+def search_by_restaurant(restaurant):
+  client = MongoClient()
+  coll = client.mockDB.reviews
+  cursor = coll.find()
+  results = []
+
+  for item in cursor:
+    if restaurant.lower() == item['name'].lower():
+      results.append(item)
+
+  return results
--- mockDB.py
+++ mockDB.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
 
 client = MongoClient()
+client.mockDB.reviews.drop()
 coll = client.mockDB.reviews
 data = [
-  {'rating': '5.0', 'body': 'What an amazing foodie experience at Fang!'},
-  {'rating': '3.0', 'body': 'The serious was relatively slow and the main guy was very pushy about his suggestions.'}
+  {'rating': '5.0', 'name': 'Fang', 'body': 'What an amazing foodie experience at Fang!'},
+  {'rating': '3.0', 'name': 'Fang', 'body': 'The serious was relatively slow and the main guy was very pushy about his suggestions.'},
+  {'rating': '5.0', 'name': 'Los Hermanos Taqueria', 'body': 'DELICIOUS! It is a little sketchy, just because it\'s literally INSIDE a liquor store, but the lady who took our order was very sweet and giving.'}
 ]
+
 for datum in data:
   coll.insert(datum)
