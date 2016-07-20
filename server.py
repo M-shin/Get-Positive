31501be444e5c27661419338292daf993cdbfc9c
Michael Shin <mshin@godaddy.com>
2016-07-19 21:41:20 -0700

    Begin styling app page. Connect server to menu

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from modelAPI import *
+from get_positive.get_positive.GetMenu import getMenuItems
 import subprocess
 import time
 
@@ -18,7 +19,6 @@ def _prefetch():
   rest_id = get_restaurant_name()
   print rest_id, url
   return render_template('loading2.html', id=rest_id, url=url)
-  #  return jsonify(**{'id': rest_id, 'url': url})
 
 @app.route('/main', methods=['GET'])
 def _appMain():
@@ -29,13 +29,27 @@ def _appMain():
 
   # Begin by scraping the reviews
   subprocess.call(['python', 'get_positive/get_positive/BeginScrape.py', '-u', url, '-n', rest_id])
+
+  # Populate database with menu items
+  getMenuItems()
+
   # Use model endpoints to access data
   score = get_score(rest_id)
   reviews = get_top_reviews(rest_id, NUM_REVIEWS)
   plates = get_top_plates(rest_id, NUM_REVIEWS)
   stars = get_review_distribution(rest_id)
 
-  return render_template('app.html', score=score, reviews=reviews, plates=plates, stars=stars)
+  return render_template('app.html', score=score, reviews=reviews, plates=plates, stars=stars, id=rest_id)
+
+@app.route('/testmain')
+def _testMain():
+  # Use model endpoints to access data
+  score = 5.0
+  reviews = ['Wow you guys are good', 'Like, real good', 'JK you suck hahahaha'];
+  plates = ['Fish Soup', 'Fish Bowl', 'Fishy'];
+  stars = {'1': 5, '2': 2, '3': 10, '4': 16, '5': 40}
+
+  return render_template('app.html', score=score, reviews=reviews, plates=plates, stars=stars, id='In-N-Out Burger')
 
 @app.route('/loading1')
 def _loading1():
