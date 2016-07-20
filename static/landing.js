3723cba92ee48fc68f793bdb22635a2c77530670
Michael Shin <mshin@godaddy.com>
2016-07-20 04:03:36 -0700

    Connect enter key to forms

--- static/landing.js
+++ static/landing.js
@@ -1,5 +1,11 @@
 // Main javascript file for landing.html
 
+$(document).keypress(function(e) {
+  if (e.which == 13) {
+    handleSubmit();
+  }
+});
+
 function handleSubmit() {
   $('#spinnerArea').load('/spinner');
 
